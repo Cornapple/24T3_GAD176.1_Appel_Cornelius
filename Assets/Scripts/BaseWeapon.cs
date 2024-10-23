b5c8fc79c5cb6e3b966bb161d17f2fde5fc30ef6
Cornapple <1031995@student.sae.edu.au>
2024-10-23 15:47:38 +1100

    imported evidence of inheritance and polymorphism

--- Assets/Scripts/BaseWeapon.cs
+++ Assets/Scripts/BaseWeapon.cs
@@ -1,10 +1,166 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BaseWeapon : MonoBehaviour
 {
-    public int magazineSize;
+    #region FUNCTIONS
+    //references ingame object/prefab which is a bullet
+    public GameObject bullet;
+
+    //the force of the bullet
+    [SerializeField] public float 
+        shootForce, 
+        upwardForce;
+
+    //gun statistics
+    [SerializeField] public float 
+        reloadTime, 
+        timeBetweenShots, 
+        timeBetweenShooting, 
+        spread;
+
+    [SerializeField] public int
+        bulletsPerClick;
+
+    protected int MagazineSize;
+
+    [SerializeField] public bool 
+        allowButtonHold;
+
+    [SerializeField] protected int 
+        bulletsInMagazine, 
+        bulletsShot;
+
+    public bool 
+        shooting, 
+        readyToShoot, 
+        reloading;
+
+    //reference to game camera and point where bullets will hit (centre of screen)
+    public Camera fpsCamera;
+    public Transform attackPoint;
+
+    //graphic references
+    public TextMeshProUGUI ammunitionDisplay;
+
+    public bool allowInvoke = true;
+
+    #endregion
+
+    #region UPDATE
+    private void Update()
+    {
+        MyInput();
+
+        //set up display Information
+        if (ammunitionDisplay != null)
+            Debug.Log("Ammunition is null");
+        //ammunitionDisplay.SetText(bulletsInMagazine / bulletsPerClick + " / " + MagazineSize / bulletsPerClick);
+        ammunitionDisplay.SetText(bulletsInMagazine + " / " + MagazineSize);
 
 
+
+        if (bulletsInMagazine <= MagazineSize && reloading)
+        {
+            bulletsInMagazine = MagazineSize;
+        }
+        else if (bulletsInMagazine >= 1)
+        {
+            MyInput();
+        }
+
+    }
+
+    #endregion
+
+
+    #region INPUT
+    public void MyInput()
+    {
+        Debug.Log("MyInput function has been called");
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (readyToShoot && shooting && !reloading && bulletsInMagazine > 0)
+        {
+            //bulletsInMagazine = 20;
+
+            Shoot();
+            Debug.Log("Shoot function has been called");
+        }
+
+        //calls reloading function if magazine is no completely full
+        if (Input.GetKeyDown(KeyCode.R) && bulletsInMagazine < MagazineSize && !reloading)
+        {
+            Reload();
+            Debug.Log("Reload function has bee called");
+        }
+
+        //if magazine is completely empty and you try to shoot, reload is called
+        if (readyToShoot && shooting && !reloading && bulletsInMagazine <= 0)
+        {
+            Reload();
+            Debug.Log("Reload function has been called");
+        }
+
+        if (bulletsInMagazine >= 0)
+        {
+            return;
+        }
+    }
+    #endregion
+
+    #region SHOOTING  
+    public virtual void Shoot()
+    {
+        Debug.Log("Shoot function is active");
+
+        //invokes the reset of shooting
+        if (allowInvoke)
+        {
+            //Invoke("ResetShot", timeBetweenShooting);
+            //allowInvoke = false;
+            ResetShot();
+        }
+
+        //for more than one bullet per click
+        if (bulletsShot < bulletsPerClick && bulletsInMagazine > 0)
+        {
+            //Invoke("Shoot", timeBetweenShots);
+            Shoot();
+        }
+    }
+    #endregion
+     
+
+    #region RELOADING   
+    public void ResetShot()
+    {
+        Debug.Log("ResetShot function is active");
+        readyToShoot = true;
+        //allowInvoke = true;
+    }
+
+    public void Reload()
+    {
+        Debug.Log("Reload function is active");
+        reloading = true;
+
+        //Invoke("ReloadFinished", reloadTime);
+        if (reloading == true)
+        {
+            ReloadingFinished();
+        }
+    }
+
+    public void ReloadingFinished()
+    {
+        Debug.Log("ReloadingFinished function is active");
+        //bulletsInMagazine = MagazineSize;
+        reloading = false;
+    }
+
+    #endregion
 }
--- Assets/Scripts/Pistol.cs
+++ Assets/Scripts/Pistol.cs
@@ -1,61 +1,37 @@
 using TMPro;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
-/// <summary>
-/// move all shared functions from pistol to baseweapon
-/// 
-/// </summary>
+
 
 public class Pistol : BaseWeapon
 {
-    #region FUNCTIONS
-    //references ingame object/prefab which is a bullet
-    public GameObject projectile;
-
-    //the force of the bullet
-    [SerializeField] private float shootForce, upwardForce;
-
-    //gun statistics
-    [SerializeField] private float reloadTime, timeBetweenShots, timeBetweenShooting, spread;
-    [SerializeField] private int MagazineSize, bulletsPerClick;
-    [SerializeField] private bool allowButtonHold;
-    [SerializeField] protected int bulletsInMagazine, bulletsShot;
-    public bool shooting, readyToShoot, reloading;
-
-    //reference to game camera and point where bullets will hit (centre of screen)
-    public Camera fpsCamera;
-    public Transform attackPoint;
-
-    //graphic references
-    public TextMeshProUGUI ammunitionDisplay;
-
-    public bool allowInvoke = true;
-
-    #endregion
+    private int pistolMagazineSize;
 
 
     private void Start()
     {
-        Debug.Log(magazineSize);
-
+        ////MagazineSize = pistolMagazineSize;
+        pistolMagazineSize = 20;
+        //bulletsInMagazine = pistolMagazineSize;
     }
-
     #region UPDATE AND AWAKE
     private void Update()
     {
+
         MyInput();
 
         //set up display Information
         if (ammunitionDisplay != null)
-            Debug.Log("Ammunition is null");
+            Debug.Log(" Pistol Ammunition is null");
         //ammunitionDisplay.SetText(bulletsInMagazine / bulletsPerClick + " / " + MagazineSize / bulletsPerClick);
-        ammunitionDisplay.SetText(bulletsInMagazine + " / " + MagazineSize);
+        ammunitionDisplay.SetText(bulletsInMagazine + " / " + pistolMagazineSize);
 
-        
 
-        if (bulletsInMagazine <= MagazineSize && reloading)
+
+        if (bulletsInMagazine <= pistolMagazineSize && reloading)
         {
-            bulletsInMagazine = MagazineSize;
+            bulletsInMagazine = pistolMagazineSize;
         }
         else if (bulletsInMagazine >= 1)
         {
@@ -68,50 +44,11 @@ public class Pistol : BaseWeapon
         bulletsInMagazine = MagazineSize;
         ReloadingFinished();
         readyToShoot = true;
-
     }
-    #endregion
 
-    #region INPUT
-    private void MyInput()
+    public override void Shoot()
     {
-        Debug.Log("MyInput function has been called");
-        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
-        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
-
-        if (readyToShoot && shooting && !reloading && bulletsInMagazine > 0)
-        {
-            //bulletsInMagazine = 20;
-
-            Shoot();
-            Debug.Log("Shoot function has been called");
-        }
-
-        //calls reloading function if magazine is no completely full
-        if (Input.GetKeyDown(KeyCode.R) && bulletsInMagazine < MagazineSize && !reloading)
-        {
-            Reload();
-            Debug.Log("Reload function has bee called");
-        }
-
-        //if magazine is completely empty and you try to shoot, reload is called
-        if (readyToShoot && shooting && !reloading && bulletsInMagazine <= 0)
-        {
-            Reload();
-            Debug.Log("Reload function has been called");
-        }
-
-        if (bulletsInMagazine >= 0 )
-        {
-            return;
-        }
-    }
-    #endregion
-
-    #region SHOOTING  
-    private void Shoot()
-    {
-        Debug.Log("Shoot function is active");
+        Debug.Log("pistol Shoot function is active");
         readyToShoot = false;
 
         Ray ray = fpsCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
@@ -130,71 +67,30 @@ public class Pistol : BaseWeapon
         float x = Random.Range(-spread, spread);
         float y = Random.Range(-spread, spread);
 
-        Vector3 directionWithSpread = directionWithoutSpread + new Vector3 (x, y, 0);
-
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0);
         //instantiate projectile
-        GameObject currentProjectile = Instantiate(projectile, attackPoint.position, Quaternion.identity);
-            
+        GameObject pistolBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+
 
         //rotates object to the direction of shooting
-        currentProjectile.transform.forward = directionWithoutSpread.normalized;
+        pistolBullet.transform.forward = directionWithoutSpread.normalized;
 
         //Add force to projectile
 
 
 
         // normal projectiles do not need upward force
-        currentProjectile.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse); // I assume this is the problem with lack of force
+        pistolBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse); // I assume this is the problem with lack of force
 
 
-        currentProjectile.GetComponent<Rigidbody>().AddForce(fpsCamera.transform.up * upwardForce, ForceMode.Impulse);
+        pistolBullet.GetComponent<Rigidbody>().AddForce(fpsCamera.transform.up * upwardForce, ForceMode.Impulse);
 
         bulletsInMagazine = bulletsInMagazine - bulletsShot; ///////
         bulletsShot++;
-
-        //invokes the reset of shooting
-        if (allowInvoke)
-        {
-            //Invoke("ResetShot", timeBetweenShooting);
-            //allowInvoke = false;
-            ResetShot();
-        }
-
-        //for more than one bullet per click
-        if (bulletsShot < bulletsPerClick && bulletsInMagazine > 0)
-        {
-            //Invoke("Shoot", timeBetweenShots);
-            Shoot();
-        }
-    }
-    #endregion
-
-    #region RELOADING   
-    private void ResetShot()
-    {
-        Debug.Log("ResetShot function is active");
-        readyToShoot = true;
-        //allowInvoke = true;
     }
 
-    private void Reload()
-    {
-        Debug.Log("Reload function is active");
-        reloading = true;
 
-        //Invoke("ReloadFinished", reloadTime);
-        if (reloading == true)
-        {
-            ReloadingFinished();
-        }
-    }
 
-    private void ReloadingFinished()
-    {
-        Debug.Log("ReloadingFinished function is active");
-        bulletsInMagazine = MagazineSize;
-        reloading = false;
-    }
-
-    #endregion
 }
+
+#endregion
